5d05405456fc2b7d8b2effde6217f904944ccbd7
unknown <sazachar@sazachar-WS.cisco.com>
2013-10-25 15:10:19 +0530

    first commit

---
+++ tab-plugin.js
@@ -0,0 +1,21 @@
+﻿(function($) {
+	$.fn.accordionPlugi = function(options) {
+		var settings = $.extend({}, options);
+		var TabLinkMain=settings.TabLinkMain;
+		var TabContentmain=settings.TabContentmain;
+		
+		$(TabContentmain + '> ').hide();
+		$(TabContentmain + '> :first-child').show();
+		$(TabLinkMain+ '> li:first-child').addClass('active');
+		
+		$(TabLinkMain+ '> li').click(function(){
+			 var liInndex=$(this).index()+1;
+			 $(TabContentmain + '> ').slideUp();
+			 $(TabLinkMain+ '> li').removeClass('active');
+			 $(this).addClass('active');
+			 $(TabContentmain + '> :nth-child('+liInndex+')').slideDown();
+		})
+	}
+}(jQuery));
+
+
