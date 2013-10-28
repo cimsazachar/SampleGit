cfaaca0ed46037b9bed7508c8f5d0ed13c8fabae
sanbin7880 <thompilgeorge@gmail.com>
2013-10-28 16:21:47 +0530

    new script folder added

---
+++ scripts/tab-plugin.js
@@ -0,0 +1,22 @@
+﻿(function($) {
+	$.fn.accordionPlugi = function(options) {
+		var settings = $.extend({}, options);
+		var TabLinkMain=settings.TabLinkMain;
+		var TabContentmain=settings.TabContentmain;
+		
+		$(TabContentmain + '> ').hide();
+		$(TabContentmain + '> :first-child').show();
+		$(TabLinkMain+ '> li:first-child').addClass('active');
+		
+		$(TabLinkMain+ '> li').click(function(){
+			 var liInndex=$(this).index()+1;
+			 $(TabContentmain + '> ').slideUp();
+			 $(TabLinkMain+ '> li').removeClass('active');
+			 $(this).addClass('active');
+			 $(TabContentmain + '> :nth-child('+liInndex+')').slideDown();
+		})
+	}
+}(jQuery));
+
+
+// -- Added comment
